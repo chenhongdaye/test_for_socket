0bdbac5af0ad471ab5505d5ff37640c8321f422e
macro-c <18942331677@163.com>
2016-03-04 11:14:07 +0800

    Update client.c

--- client_things/client.c
+++ client_things/client.c
@@ -1,6 +1,7 @@
 #include"header.h"
 
 struct message string_op(char*);
+void receive(struct online);
 
 int main(void)
 {
@@ -21,6 +22,31 @@ int main(void)
     exit(1);
   }
   
+  label1: //每当登录信息失败时,循环.........................................
+  puts("清输入 自己的账户名和密码,形式 id:password");
+  char * login_input;
+  gets(login_input);
+  login login_str=string_op_login(login_input);
+  
+  ///登录之后  先发送一条登录信息 接受服务器验证信息;
+  if(send(socketfd,(void*)(&login_str),sizeof(login),0)==-1)
+  {
+    puts(" send failed \n");
+    exit(1);
+  }
+  
+  int success=0;
+  if(recv(socketfd,(void*)(&success),sizeof(int))==-1)
+  {
+    puts(" shit recv failed \n");
+    exit(1);
+  }
+  char *output=((int)success==1?"登录成功! \n":"账户/密码错误,请重新输入!\n");
+  puts(output);
+  if(success==0)
+    goto label1;
+  
+  /*  未实现  发送全部在线人消息.
   puts("连接成功: \n");
   puts("当前在线的人有:\n");
   FILE *f_online=fopen(ONLINE,"r");
@@ -30,18 +56,66 @@ int main(void)
   {
     puts((struct online)usr->id);
   }
+  fclose(f_online);
+  */
+  
   puts("\n 现在可以输入了 :(格式  id:message)\n ");
   struct message message;
   char *input;
-  while(1)
+  
+  pthread_t thread;
+  struct online online1;
+  online1.sockfd=socketfd;
+  pthread_mutex_t mutex;
+  pthread_mutex_init(mutex);
+  online1.mutex=mutex;
+  if(pthread_create(&thread,NULL,(void*)(&receive),(void*)(&online1))==-1)
+  {
+    puts(" 创建线程失败 \n");
+    exit(1);
+  }
+
+  while(1)    //进入聊天  .......当前位置: 发送消息线程
   {
     gets(input); //以字串形式输入;
     struct message buf=string_op(input);
+    if(input=="logout")
+    {
+      /////////////////////////////////////////////////////////////////////////////未
+      break;
+    }
+    buf.time=time(NULL);
+    pthread_mutex_lock(mutex);
     if(send(socketfd,(void*)buf,MES_LEN,0)==-1)
     {
       puts("send failed \n");
     }
-    puts("\n发送成功\n");
+    pthread_mutex_unlock(mutex);
+    puts("\n发送成功\n 请输入:\n");
   }
   return 0;
+}
+
+void receive(struct online temp)
+{
+  int sockfd=temp.sockfd;
+  pthread_mutex_t mutex=temp.mutex;
+  struct message message;
+  while(1)
+  {
+    pthread_mutex_lock(mutex);
+    if(recv(sockfd,(void*)(&message),sizeof(struct message))==-1)
+    {
+      puts("");
+      exit(1);
+    }
+    pthread_mutex_unlock(mutex);
+    puts("from:"");
+    puts(message.id);
+    puts("\n message is:");
+    puts(message.message);
+    puts("\n");
+    puts("time:")
+    puts(ctime(message.time));
+  }
 }
