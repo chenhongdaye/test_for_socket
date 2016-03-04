1972b5b37be1b641b7baf227a9e710db8dd48704
macro-c <18942331677@163.com>
2016-03-04 11:36:00 +0800

    Update header.h
    吃饭去

--- header.h
+++ header.h
@@ -15,6 +15,8 @@
 #define ID_LEN 10;
 #define MES_LEN 50;
 #define KEY_LEN 10;
+#define SUCC 1;
+#define FAIL 0;
 #define SERVER_IP "....."
 #define SERVER_PORT ..... //短整形
 #define ONLINE "....."
@@ -22,7 +24,7 @@
 #define USRS "....."
 
 
-struct message //发送的消息or离线文件
+struct message //发送的消息or离线文件  相应的ID 为接收或者发送;
 {
   char id[ID_LEN]; //最长10
   char message[MES_LEN]; //最长50
@@ -33,6 +35,15 @@ struct online //在线文件夹
 {
   char id[ID_LEN];
   int sockfd;
+  pthread_mutex_t mutex; //新增加, 用于保证发送消息原子性;
+};
+
+struct send //用于 传递给服务器中 线程的参数
+{
+  int sockfd;
+  pthread_mutex_t mutex;
+  char id_from[ID_LEN]; //当前id
+  char id_to[ID_LEN]; 
 };
 
 struct users //用户信息 用户名和密码
@@ -41,9 +52,22 @@ struct users //用户信息 用户名和密码
   char key[KEY_LEN];
 };
 
+typedef struct users login;
+
+login string_op_login(char *string)  //从登录信息分离出消息
+{
+  return ....;
+}
+
 struct message string_op(char *input) ////////////待 从发送消息分离出 信息
 {
   return ....;
+}
+
+int init_user_file(char filename)
+{
+  filename=USRS;
+  //.........................
 }
 
 
