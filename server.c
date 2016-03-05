61c9a634004190c3c660d92095bb041e33230ad7
macro-c <18942331677@163.com>
2016-03-05 13:57:57 +0800

    Update server.c

--- server.c
+++ server.c
@@ -49,20 +49,23 @@ int main(void)
       exit(1);
     }
     
-    FILE *usr_file;
+    FILE *file_usr;
     if(file_usr=(fopen(USRS,"r"))==NULL)
     {
       puts(" 用户文件打开失败 请检查 \n");
       exit(1);
     }
-    
+    //char id[ID_LEN];
+    //char key[KEY_LEN];
+    //id 直接就是 message中id; key需要从message中提取;
+    //strncpy(key,login_message.message,KEY_LEN);
     struct users compare;
     int succ=0;
     while((fread((void*)(&compare),sizeof(struct users),1,file_usr))==1)
     {
-      char key_temp[KEY_LEN];
-      memcpy(key_temp,(&login_message)+ID_LEN*sizeof(char),KEY_LEN*sizeof(char));
-      if(strcmp(login_message.id,compare.id)==0&&strcmp(key_temp,compare.key)==0)
+      //char key_temp[KEY_LEN];
+      //memcpy(key_temp,(&login_message)+ID_LEN*sizeof(char),KEY_LEN*sizeof(char));
+      if(strcmp(login_message.id,compare.id)==0&&strncmp(login_message.message,compare.key,KEY_LEN)==0)
       {///表明登录信息正确;
         struct message success;
         success.id={'1','1','1','1','1','1','1','1','1','1'};
@@ -78,8 +81,9 @@ int main(void)
     close(file_usr);
     if(succ==0)/////////////////////////////////////////////////////////////改.......................
     {//表明登录信息 错误
-      int a=FAIL;
-      send(new_sockfd,(void*)(&a),sizeof(int));
+      struct message fail;
+      fail.id[0]='0';
+      send(new_sockfd,(void*)(&fail),sizeof(struct message));
       goto label1;
     }
     
@@ -130,143 +134,146 @@ int main(void)
     pthread_detach(thread);
     
   }
-  
-  void connect_handle(thread_argu temp) ///////此处传递形参  用于线程
+  return 0;
+}
+
+
+
+
+void connect_handle(thread_argu temp) ///////此处传递形参  用于线程
+{
+  int sockfd=temp.sockfd;
+  pthread_mutex_t mutex=temp.mutex;
+  char id_from[ID_LEN];
+  strncpy(id_from,temp.id,MES_LEN);
+  struct message message; //用于发送的消息
+  FILE *online_file;
+  while(1)
   {
-    int sockfd=temp.sockfd;
-    pthread_mutex_t mutex=temp.mutex;
-    char id_from[ID_LEN];
-    memcpy(id_from,temp.id,MES_LEN*(sizeof(char)));
-    struct message message; //用于发送的消息
-    FILE *online_file;
-    while(1)
+    pthread_mutex_lock(mutex);
+    if(recv(sockfd,(void*)(&message),sizeof(struct message),0)==-1)
     {
-      pthread_mutex_lock(mutex);
-      if(recv(sockfd,(void*)(&message),sizeof(struct message),0)==-1)
+      puts("sth is wrong!!1111111111111111111\n");
+      exit(1);
+    }
+    pthread_mutex_unlock(mutex);
+    char a[MES_LEN],b[MES_LEN];
+    char *c="logout";
+    //memcpy(a,c,6);
+    //memcpy(b,mesage.mesage,6);
+    if(strncmp(c,message.message,strlen(c))==0)
+    {
+      //用户下线消息;/////发送下线通知
+      FILE * file_out;
+      if((file_out=fopen(ONLINE,"r+"))==NULL) //读写方式打开
       {
-        puts("sth is wrong!!1111111111111111111\n");
-        exit(1);
-      }
-      pthread_mutex_unlock(mutex);
-      char a[MES_LEN],b[MES_LEN];
-      char *c="logout";
-      memcpy(a,c,6);
-      memcpy(b,mesage.mesage,6);
-      if(strcmp(a,b)==0)
-      {
-        //用户下线消息;/////发送下线通知
-        FILE * file_out;
-        if((file_out=fopen(ONLINE,"r+"))==NULL) //读写方式打开
+        //int label=0;
+        //int local=0;
+        int label2=0;
+        rewind(file_out);
+        struct online temp;
+        struct online save[10]; //存放用户信息
+        while(fread((void*)(&temp),sizeof(struct online),1,file_out)==1)//未找到更好地方式更新文件;此处:全部读取然后覆盖;
         {
-          //int label=0;
-          //int local=0;
-          int label2=0;
-          rewind(file_out);
-          struct online temp;
-          struct online save[10]; //存放用户信息
-          while(fread((void*)(&temp),sizeof(struct online),1,file_out)==1)//未找到更好地方式更新文件;此处:全部读取然后覆盖;
+          //++label;
+          ++label2;
+          *(save+labe2-1)=temp;
+          if(strcmp(id_from,temp.id)==0)
           {
-            //++label;
-            ++label2;
-            *(save+labe2-1)=temp;
-            if(strcmp(id_from,temp.id)==0)
-            {
-              --label2;   //下次会覆盖;
-              //local=labe2+1; //记录 位置;
-            }
+            --label2;   //下次会覆盖;
+            //local=labe2+1; //记录 位置;
           }
-          int count=label2+1;
-          fclose(file_out);
-          FILE* file_out2;
-          if((file_out2=fopen(ONLINE,"w+"))==NULL) //此方式打开,原文件全部清空;
-          {
-            if(fwrite((void*)(&save[i]),sizeof(),count,file_out2)<count)
-            {
-              puts("there is sth wrong!!!更新文件!!!2222222222222\n");
-              exit(1);
-            }
-          }
-          fclose(file_out2);
         }
-        close(sockfd);
-        FILE *out_message;
-        if((out_message=fopen(ONLINE,"r"))==NULL)
+        int count=label2+1;
+        fclose(file_out);
+        FILE* file_out2;
+        if((file_out2=fopen(ONLINE,"w+"))==NULL) //此方式打开,原文件全部清空;
         {
-          puts(" 这里毛病ksjdhgkjsdh \n");
-          exit(1);
-        }
-        struct online send_to;
-        while(fread((void*)(send_to),sizeof(struct online),1,out_message)==1)
-        {
-          struct message message_to;
-          char *say="已下线\n";
-          strcpy(message_to.message,say,sizeof(say)/sizeof(char));
-          message_to.time=time(NULL);
-          if(send(message_to.sockfd,(void*)(message_to),sizeof(struct message),0)==-1)
+          if(fwrite((void*)(&save[i]),sizeof(),count,file_out2)<count)
           {
-            puts(" 发不出去了\n");
+            puts("there is sth wrong!!!更新文件!!!2222222222222\n");
             exit(1);
           }
         }
-        fclose(out_message);
-        pthread_exit(0);
+        fclose(file_out2);
       }
-      char id_to[ID_LEN];
-      memcpy(id_to,message.id,MES_LEN*sizeof(char));///发往的用户  用于在线文件中检索 socket和 thread
-      memcpy(message.id,id_from,MES_LEN);  ///只需要改动接收到信息的 id部分;
-      if((file_online=fopen(ONLINE,"r"))==NULL)
+      close(sockfd);
+      FILE *out_message;
+      if((out_message=fopen(ONLINE,"r"))==NULL)
       {
-        puts(" 打开文件失败 \n");
+        puts(" 这里毛病ksjdhgkjsdh \n");
         exit(1);
       }
-      int count=0;
-      struct online usr;
-      int label=0;
-      int sockfd;
-      pthread_mutex_t mutex;
-      while((count=fread((void*)(&usr),sizeof(struct online),1,file_online))==1)
+      struct online send_to;
+      while(fread((void*)(send_to),sizeof(struct online),1,out_message)==1)
       {
-        if(usr.id==id_to)  //发送往id 在线
+        struct message message_to;
+        char *say="已下线\n";
+        strncpy(message_to.message,say,strlen(say));
+        //message_to.time=time(NULL); //原信息中已经确定;
+        if(send(message_to.sockfd,(void*)(message_to),sizeof(struct message),0)==-1)
         {
-          sockfd=usr.sockfd;
-          mutex=usr.mutex;
-          label=1;
-          break;
-        }
-      }
-      close(file_online);
-      
-      if(label==1) //在线发送;
-      {
-        pthread_mutex_lock(mutex);
-        if(send(sockfd,(void*)(&message),sizeof(struct message),0)==-1)
-        {
-          puts(" failed !\n");
-          exit(1);
-        }
-        pthread_mutex_unlock(mutex);
-      }
-      
-      else  //离线 写入离线文件;
-      {
-        FILE *offline;
-        /////////////////////////////////////////////////////////////////未实现上线接收离线消息;
-        if((offline=fopen(OFFLINE,"a+"))==NULL)   //更新离线消息文件
-        {
-          puts(" open file failed! \n");
+          puts(" 发不出去了\n");
           exit(1);
         }
-        if(fwrite((void*)(&message),sizeof(struct message),1,offline)==0)
-        {
-          puts("写入失败 \n");
-          exit(1);
-        }
-        close(offline);
       }
+      fclose(out_message);
+      pthread_exit(0);
+    } //结束 用户下线,更新文件,发送下线信息;
+    
+    
+    char id_to[ID_LEN];
+    memcpy(id_to,message.id,MES_LEN*sizeof(char));///发往的用户  用于在线文件中检索 socket和 thread
+    memcpy(message.id,id_from,MES_LEN);  ///只需要改动接收到信息的 id部分;
+    if((file_online=fopen(ONLINE,"r"))==NULL)
+    {
+      puts(" 打开文件失败 \n");
+      exit(1);
+    }
+    int count=0;
+    struct online usr;
+    int label=0;
+    int sockfd;
+    pthread_mutex_t mutex;
+    while((count=fread((void*)(&usr),sizeof(struct online),1,file_online))==1)
+    {
+      if(strcmp(usr.id,id_to)==0)  //发送往id 在线
+      {
+        sockfd=usr.sockfd;
+        mutex=usr.mutex;
+        label=1;
+        break;
+      }
+    }
+    fclose(file_online);
+    
+    if(label==1) //在线发送;
+    {
+      pthread_mutex_lock(mutex);
+      if(send(sockfd,(void*)(&message),sizeof(struct message),0)==-1)
+      {
+        puts(" failed !\n");
+        exit(1);
+      }
+      pthread_mutex_unlock(mutex);
+    }
+      
+    else  //离线 写入离线文件;
+    {
+      FILE *offline;
+      /////////////////////////////////////////////////////////////////未实现上线接收离线消息;
+      if((offline=fopen(OFFLINE,"a+"))==NULL)   //更新离线消息文件
+      {
+        puts(" open file failed! \n");
+        exit(1);
+      }
+      if(fwrite((void*)(&message),sizeof(struct message),1,offline)==0)
+      {
+        puts("写入失败 \n");
+        exit(1);
+      }
+      close(offline);
     }
   }
   
-  
-  
-  return 0;
 }
